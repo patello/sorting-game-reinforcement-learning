5b163be899040d132eab45f6abc949a7334ad8d5
Patrik Ekenberg <Patrik@Ekenberg.com>
2020-08-21 21:36:52 +0000

    Utilities module incl run_batch and training funs

---
+++ gamenet/tests/test_utilities.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+
+from ..utilities import run_batch, train
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+@pytest.mark.parametrize("test_data",[
+    {"model_name":"example_model.mx","state_fun":"int"},
+    {"model_name":"example_model_encoded.mx","state_fun":"encoded"},
+    ])
+class TestUtilities:
+    def test_run_batch(self,test_data):
+        run_batch(model_path=file_path+"/resources/"+test_data["model_name"],batches=5,state_fun=test_data["state_fun"])
+
+    def test_train(self,test_data):
+        train(state_fun=test_data["state_fun"],batch_size=2,batches=2)
---
+++ gamenet/utilities.py
@@ -0,0 +1,32 @@
+from .model import ActorCritic
+from .agent import Agent
+from .nn_runner import NNRunner
+from .gameai import GameAI
+
+import torch
+import os
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+def run_batch(model_path, batches=1000, state_fun="encoded", empty_pos_indicator = -41):
+    model = torch.load(model_path)
+    agent = Agent(model=model,default_action_selection="Max")
+    game_runner = GameAI(empty_pos_indicator=empty_pos_indicator,state_fun=state_fun)
+    nnrunner = NNRunner(agent,game_runner)
+    nnrunner.run_batch(batches)
+
+def train(model=None,model_path=None,result_path=None, state_fun="encoded", empty_pos_indicator = -41, hidden_size=200, batch_size=10,batches=10000,learning_rate=3e-5, gamma=0.9, entropy_loss_coeff=0.01):
+    if state_fun=="encoded":
+        input_size=816
+    elif state_fun=="int":
+        input_size=20
+    if model!=None:
+        model=torch.load(file_path+model_path)
+    else:
+        model = ActorCritic(input_size,16,hidden_size=hidden_size)
+    agent = Agent(model=model,learning_rate=learning_rate, gamma=gamma, entropy_loss_coeff=entropy_loss_coeff)
+    game_runner = GameAI(empty_pos_indicator=empty_pos_indicator,state_fun=state_fun)
+    nnrunner = NNRunner(agent,game_runner)
+    #model_path argument tells the nnrunner where to save the trained model, relative to current directory (saved every 1000th batch)
+    #result_path argument tells nnrunner where to save a csv file with training data over time, otherwise outputs to terminal
+    nnrunner.train(batch_size=batch_size,batches=batches,model_path=model_path,result_path=result_path)
